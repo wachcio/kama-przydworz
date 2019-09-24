c0ac9fe842e90824fe1828c2c749ac9f1e262379
wachcio <wachcio@poczta.fm>
2019-09-24 02:26:14 +0200

    lang pl

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,13 @@ export default {
   /*
    ** Headers of the page
    */
+
   head: {
+    htmlAttrs: {
+      lang: 'pl'
+    },
     title: process.env.npm_package_name || '',
+
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
