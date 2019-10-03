04d697597075407fc654a499670ae2f5e10d3093
wachcio <wachcio@poczta.fm>
2019-10-04 00:13:56 +0200

    add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,21 @@ export default {
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        size: '16x16',
+        href: '/favicon-16x16.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        size: '32x32',
+        href: '/favicon-32x32.png'
+      }
+    ]
   },
   /*
    ** Customize the progress-bar color
