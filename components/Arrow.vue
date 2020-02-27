30a6e0c33e15119c01266beb32411bf32a898243
wachcio <wachcio@poczta.fm>
2020-02-27 02:15:52 +0100

    check touch devices

--- components/Arrow.vue
+++ components/Arrow.vue
@@ -1,5 +1,11 @@
 <template>
-  <a href="#" v-scroll-to="'#top'" class="arrow" title="Przewiń na górę strony">
+  <a
+    href="#"
+    v-scroll-to="{ el: '#top', onDone: onDone }"
+    class="arrow"
+    :class="isAnimation ? 'arrowAnimation' : ''"
+    title="Przewiń na górę strony"
+  >
     <fa :icon="['fas', 'angle-double-up']" class="arrow__icon" />
   </a>
 </template>
@@ -8,12 +14,46 @@
 export default {
   name: 'Arrow',
   data() {
-    return {}
+    return {
+      isAnimation: true
+    }
   },
   components: {},
-  methods: {},
+  methods: {
+    onDone() {
+      console.log('koniec')
+    },
+    is_touch_device() {
+      var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ')
+      var mq = function(query) {
+        return window.matchMedia(query).matches
+      }
+
+      if (
+        'ontouchstart' in window ||
+        (window.DocumentTouch && document instanceof DocumentTouch)
+      ) {
+        return true
+      }
+
+      // include the 'heartz' as a way to have a non matching MQ to help terminate the join
+      // https://git.io/vznFH
+      var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join(
+        ''
+      )
+      return mq(query)
+    }
+  },
   computed: {},
-  created() {},
+  mounted() {
+    if (this.is_touch_device()) {
+      // alert('Your device is Touch')
+      this.isAnimation = false
+    } else {
+      // alert('Your device is NOT touch')
+      this.isAnimation = true
+    }
+  },
   watch: {}
 }
 </script>
@@ -35,12 +75,14 @@ export default {
   &:hover {
     opacity: 1;
   }
-  &:hover .arrow__icon {
-    // transform: translate(-50%, -150%);
-    animation-duration: 0.7s;
-    animation-name: arrowUp;
-    animation-iteration-count: infinite;
-  }
+}
+
+.arrowAnimation:hover .arrow__icon {
+  // transform: translate(-50%, -150%);
+
+  animation-duration: 0.7s;
+  animation-name: arrowUp;
+  animation-iteration-count: infinite;
 }
 
 @keyframes arrowUp {
